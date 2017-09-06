a155472a18566414e877a3a4cdd8c9fc818fe75d
Pierre Binauld <pierre.binauld@gmail.com>
2017-09-06 18:44:15 +0200

    Add CursorLiveData which close cursor when needed

---
+++ app/src/main/java/org/lagonette/android/app/arch/CursorLiveData.java
@@ -0,0 +1,36 @@
+package org.lagonette.android.app.arch;
+
+import android.database.Cursor;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.concurrent.Executor;
+
+public class CursorLiveData
+        extends DbLiveData<Cursor> {
+
+    @Nullable
+    private Cursor mCursor;
+
+    public CursorLiveData(
+            @NonNull String[] tables,
+            @NonNull Executor executor,
+            @NonNull DataLoader<Cursor> dataLoader) {
+        super(
+                tables,
+                executor,
+                dataLoader
+        );
+    }
+
+    @Override
+    @MainThread
+    public void setValue(Cursor newCursor) {
+        if (mCursor != null && !mCursor.isClosed()) {
+            mCursor.close();
+        }
+        mCursor = newCursor;
+        super.setValue(newCursor);
+    }
+}
--- app/src/main/java/org/lagonette/android/repo/MainRepo.java
+++ app/src/main/java/org/lagonette/android/repo/MainRepo.java
@@ -5,6 +5,8 @@ import android.arch.lifecycle.Transformations;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
+import org.lagonette.android.app.arch.CursorLiveData;
+import org.lagonette.android.app.arch.LiveEvent;
 import org.lagonette.android.app.arch.DbLiveData;
 import org.lagonette.android.locator.DB;
 import org.lagonette.android.room.database.LaGonetteDatabase;
@@ -20,7 +22,8 @@ import java.util.concurrent.Executor;
 
 public class MainRepo {
 
-    @NonNull Context mContext;
+    @NonNull
+    private Context mContext;
 
     @NonNull
     private final Executor mExecutor;
@@ -37,41 +40,42 @@ public class MainRepo {
         return new LambdaResourceAlgorithm<>(
                 mExecutor,
                 this::shouldFetch,
-                () -> Transformations.switchMap(
-                        searchLiveEvent,
-                        search -> new DbLiveData<>(
-                                Tables.TABLES,
-                                mExecutor,
-                                () -> MapPartnerReader.create( // TODO Fix "Application did not close the cursor or database object that was opened here" issue
-                                        DB
+                () -> Transformations.map(
+                        Transformations.switchMap(
+                                searchLiveEvent,
+                                search -> new CursorLiveData(
+                                        Tables.TABLES,
+                                        mExecutor,
+                                        () -> DB
                                                 .get()
                                                 .mainDao()
                                                 .getMapPartner(SearchUtil.formatSearch(search))
                                 )
-
-                        )
+                        ),
+                        MapPartnerReader::create
                 ),
                 () -> new DataRefreshWorker(mContext)
         )
                 .start()
                 .getAsLiveData();
-    }
+    } // TODO Fix "Application did not close the cursor or database object that was opened here" issue
 
     public LiveData<Resource<PartnerDetailReader>> getPartnerDetail(@NonNull LiveData<Long> partnerIdLiveData) {
         return new LambdaResourceAlgorithm<>(
                 mExecutor,
                 this::shouldFetch,
-                () -> Transformations.switchMap(
-                        partnerIdLiveData,
-                        partnerId -> new DbLiveData<>(
-                                Tables.TABLES,
-                                mExecutor,
-                                () -> PartnerDetailReader.create(
-                                        partnerId > Statement.NO_ID
+                () -> Transformations.map(
+                        Transformations.switchMap(
+                                partnerIdLiveData,
+                                partnerId -> new CursorLiveData(
+                                        Tables.TABLES,
+                                        mExecutor,
+                                        () -> partnerId > Statement.NO_ID
                                                 ? DB.get().mainDao().getPartnerDetail(partnerId)
                                                 : null
                                 )
-                        )
+                        ),
+                        PartnerDetailReader::create
                 ),
                 () -> new DataRefreshWorker(mContext)
         )
@@ -83,18 +87,19 @@ public class MainRepo {
         return new LambdaResourceAlgorithm<>(
                 mExecutor,
                 this::shouldFetch,
-                () -> Transformations.switchMap(
-                        searchLiveData,
-                        search -> new DbLiveData<>(
-                                Tables.TABLES,
-                                mExecutor,
-                                () -> FilterReader.create(
-                                        DB
+                () -> Transformations.map(
+                        Transformations.switchMap(
+                                searchLiveData,
+                                search -> new CursorLiveData(
+                                        Tables.TABLES,
+                                        mExecutor,
+                                        () -> DB
                                                 .get()
                                                 .mainDao()
                                                 .getFilters(SearchUtil.formatSearch(search))
                                 )
-                        )
+                        ),
+                        FilterReader::create
                 ),
                 () -> new DataRefreshWorker(mContext)
         )
