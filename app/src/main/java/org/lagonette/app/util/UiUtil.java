16bd086f4947399f74cfee34660a4469dfa66da2
Pierre Binauld <pierre.binauld@gmail.com>
2017-12-29 15:46:00 +0100

    Fix UiUtil

--- app/src/main/java/org/lagonette/app/util/UiUtil.java
+++ app/src/main/java/org/lagonette/app/util/UiUtil.java
@@ -1,14 +1,17 @@
 package org.lagonette.app.util;
 
 import android.content.res.Resources;
+import android.os.Build;
 
 public class UiUtil {
 
     public static int getStatusBarHeight(Resources resources) {
         int result = 0;
-        int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = resources.getDimensionPixelSize(resourceId);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = resources.getDimensionPixelSize(resourceId);
+            }
         }
         return result;
     }
