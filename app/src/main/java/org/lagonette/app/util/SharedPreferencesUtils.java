667c8b2ed848b2feb0df77c5b1b25f26ba0d7c51
Pierre Binauld <pierre.binauld@gmail.com>
2018-04-25 16:44:33 +0200

    Show Gonette footprint on first launch and on my position long click

--- app/src/main/java/org/lagonette/app/util/SharedPreferencesUtils.java
+++ app/src/main/java/org/lagonette/app/util/SharedPreferencesUtils.java
@@ -12,11 +12,11 @@ public class SharedPreferencesUtils {
 
 	public static final String PREFERENCE_START_BEARING = "preference_start_bearing";
 
-	public static final float DEFAULT_VALUE_START_LATITUDE = 45.764043f;
+	public static final float DEFAULT_VALUE_START_LATITUDE = 45.86691680129967f;
 
-	public static final float DEFAULT_VALUE_START_LONGITUDE = 4.835659f;
+	public static final float DEFAULT_VALUE_START_LONGITUDE = 4.810781255364418f;
 
-	public static final float DEFAULT_VALUE_START_ZOOM = 10;
+	public static final float DEFAULT_VALUE_START_ZOOM = 8.917425f;
 
 	public static final float DEFAULT_VALUE_START_TILT = 0;
 
