57c45803f060dc0725474c6859e727a995bd390f
Pierre Binauld <pierre.binauld@gmail.com>
2018-05-03 14:54:52 +0200

    Ensure shortcut is displayed correctly when there is no HQ

--- app/src/main/java/org/lagonette/app/room/statement/ShortcutStatement.java
+++ app/src/main/java/org/lagonette/app/room/statement/ShortcutStatement.java
@@ -5,22 +5,37 @@ import org.lagonette.app.room.statement.base.GonetteStatement;
 public abstract class ShortcutStatement
 		implements GonetteStatement {
 
-	public static final String SQL =
-			"SELECT * " +
-					" FROM (" +
-					" SELECT location.id AS headquarter_location_id, partner.logo AS headquarter_icon " +
+	public static final String CONDITION_SQL =
+			" SELECT 1" +
 					FROM_PARTNER +
-					JOIN_LOCATION_ON_PARTNER +
-					" WHERE partner.is_gonette_headquarter = 1 " +
-					" LIMIT 1" +
-					" ), (" +
-					" SELECT " +
+					" LIMIT 1";
+
+	public static final String SHORTCUT_SQL =
+			" SELECT " +
 					" SUM(location_metadata.is_visible) as visible_partner_count, " +
 					" COUNT(1) AS partner_count, " +
 					" SUM(CASE WHEN location.is_exchange_office > 0 THEN location_metadata.is_visible ELSE 0 END) AS visible_exchange_office_count, " +
 					" SUM(location.is_exchange_office) AS exchange_office_count " +
 					FROM_LOCATION +
-					JOIN_METADATA_ON_LOCATION +
+					JOIN_METADATA_ON_LOCATION;
+
+	public static final String HEADQUARTER_SQL =
+			" SELECT location.id AS headquarter_location_id, partner.logo AS headquarter_icon " +
+					FROM_PARTNER +
+					JOIN_LOCATION_ON_PARTNER +
+					" WHERE partner.is_gonette_headquarter = 1 " +
+					" LIMIT 1";
+
+	public static final String SQL =
+			"SELECT * " +
+					" FROM (" +
+					CONDITION_SQL +
+					" )" +
+					" LEFT JOIN (" +
+					SHORTCUT_SQL +
+					" )" +
+					"LEFT JOIN (" +
+					HEADQUARTER_SQL +
 					" ) " +
 					" LIMIT 1";
 
